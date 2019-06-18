ea05c5c366abf3454f14be3ccfbbf37beb80236b
Julien Bisconti <julien.bisconti@gmail.com>
2019-06-18 15:46:00 +0200

    Add private key output

--- modules/gcp-project-service-account/outputs.tf
+++ modules/gcp-project-service-account/outputs.tf
@@ -6,3 +6,7 @@ output "service_account_email" {
   value = google_service_account.app.email
 }
 
+output "service_account_private_key" {
+  sensitive = true
+  value     = google_service_account_key.app.private_key
+}
